29fb08ae3e407e9629fbd9ec732f3c4dd5972589
Jogeonsang <rhcn258@gmail.com>
2019-01-04 22:40:38 +0900

    gameInfo 섹션 완료 ( 스타일 수정 필요)

--- src/components/Summoner/components/gameList/gameInfo/index.js
+++ src/components/Summoner/components/gameList/gameInfo/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import moment from "moment";
+import "moment/locale/ko";
 
-const GameInfo = ({
-}) => {
-// const { championId, spell1Id, spell2Id, stats} = userList;
-// const { assists, champLevel, deaths, kills, totalMinionsKilled, neutralMinionsKilled } = stats;
-return (
-        <div>GameInfo</div>
-    )
-}
+import styles from "./styles.module.scss";
+moment.locale("ko");
+const GameInfo = ({ queueId, gameCreation, gameDuration, winlose }) => {
+  const win = winlose ? "승리" : "패배";
+  const queue = queueId === 440 ? "자유 5:5 랭크" : "솔랭";
+  return (
+    <div className={styles.championInfo}>
+      <div className={styles.queue}>{queue}</div>
+      <div className={styles.gameCreation}>{`${moment(
+        gameCreation
+      ).fromNow()}`}</div>
+      <div className={styles.bar} />
+      <div className={styles.winlose}>{win}</div>
+      <div className={styles.gameDuration}>{`${moment({})
+        .seconds(gameDuration)
+        .format("mm분 ss초")}`}</div>
+    </div>
+  );
+};
 
-export default GameInfo
+export default GameInfo;
--- src/components/Summoner/components/gameList/index.js
+++ src/components/Summoner/components/gameList/index.js
@@ -1,81 +1,79 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import styles from './styles.module.scss';
-import { getDetailGameList } from 'redux/Summoner/gameList/action';
-import ChampionInfo from './championInfo';
-import GameInfo from './gameInfo';
+import React from "react";
+import { connect } from "react-redux";
+import styles from "./styles.module.scss";
+import { getDetailGameList } from "redux/Summoner/gameList/action";
+import ChampionInfo from "./championInfo";
+import GameInfo from "./gameInfo";
 const win = {
-    backgroundColor : '#A2CFEC'
-}
+  backgroundColor: "#A2CFEC"
+};
 const lose = {
-    backgroundColor :  '#E2B5B3'
-}
+  backgroundColor: "#E2B5B3"
+};
 class GameList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            gameList: [],
-            teamId: null,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameList: [],
+      teamId: null
+    };
+  }
 
-    componentDidMount() {
-        const { matches } = this.props
-        matches.map(game => {
-            const { gameId } = game
-            this.props.getDetailGameList(gameId)
-        })
-    }
+  componentDidMount() {
+    const { matches } = this.props;
+    matches.map(game => {
+      const { gameId } = game;
+      this.props.getDetailGameList(gameId);
+    });
+  }
 
-    renderDiv() {
-        if(this.props.gameList.gameInfo.length === 10) {
-            return this.props.gameList.gameInfo.map(gameList => {
-                const { participantIdentities, participants } = gameList
-                const participant = participantIdentities.filter(userList => {
-                    if(userList.player.summonerName === this.props.summonerInfo.name) {
-                        return userList.participantId;
-                    }
-                });
-                const userInfo = participants.filter(userList => {
-                    if(participant[0].participantId === userList.participantId) {
-                        return userList
-                }
-                });
-                const winloseStyle = userInfo[0].stats.win ? win : lose;
-                return (
-                    <div className={styles.gameList}>
-                        <div className={styles.winlose} style={winloseStyle}>
-                      <GameInfo />
-                      <ChampionInfo 
-                        userinfo={userInfo}
-                        participants={participants}
-                        participant={participant}
-                      />
-                      </div>
-                    </div>
-                )
-            });
-        } else {
-            return (
-                <p>로딩중입니다</p>
-            );
-        };
-    }
-
-    render() {
+  renderDiv() {
+    if (this.props.gameList.gameInfo.length === 10) {
+      return this.props.gameList.gameInfo.map(gameList => {
+        const { participantIdentities, participants } = gameList;
+        const participant = participantIdentities.filter(userList => {
+          if (userList.player.summonerName === this.props.summonerInfo.name) {
+            return userList.participantId;
+          }
+        });
+        const userInfo = participants.filter(userList => {
+          if (participant[0].participantId === userList.participantId) {
+            return userList;
+          }
+        });
+        const winloseStyle = userInfo[0].stats.win ? win : lose;
         return (
-          <div>
-              {this.renderDiv()}
-          </div>  
+          <div className={styles.gameList}>
+            <div className={styles.winlose} style={winloseStyle}>
+              <GameInfo
+                queueId={gameList.queueId}
+                gameCreation={gameList.gameCreation}
+                gameDuration={gameList.gameDuration}
+                winlose={userInfo[0].stats.win}
+              />
+              <ChampionInfo
+                userinfo={userInfo}
+                participants={participants}
+                participant={participant}
+              />
+            </div>
+          </div>
         );
+      });
+    } else {
+      return <p>로딩중입니다</p>;
     }
+  }
+
+  render() {
+    return <div>{this.renderDiv()}</div>;
+  }
 }
 export default connect(
-    ({ summoner}) => 
-    ({
-        gameList: summoner.game,
-    }),
-    {
-        getDetailGameList,
-    }
-    )(GameList)
+  ({ summoner }) => ({
+    gameList: summoner.game
+  }),
+  {
+    getDetailGameList
+  }
+)(GameList);
