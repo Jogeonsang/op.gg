1dc49659abadf795d7fe132a350ad39df2c66766
Jogeonsang <rhcn258@gmail.com>
2019-01-12 19:15:32 +0900

    리팩토링?

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,53 +1,57 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 // src modules
-import { getSummoner } from 'redux/Summoner/search/action';
-import styles from './styles.module.scss';
-
+import { getSummoner } from "redux/Summoner/search/action";
+import styles from "./styles.module.scss";
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            redirect: '',
-            summonerName: '',
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: "",
+      summonerName: ""
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.summonerInfo) {
+      this.setState({ redirect: { to: "summoner" } });
     }
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.summonerInfo) {
-            this.setState({ redirect: { to: 'summoner' }});
-        }
-    }
-    searchSummoner = (e) => {
-        e.preventDefault();
-        const { summonerName } = this.state
-        this.props.getSummoner(summonerName)
-    }
-    handleInputChange = (e) => {
-        this.setState({summonerName: e.target.value});
-    }
-    render() {
-        const { redirect } = this.state;
-        if (redirect) return <Redirect {...redirect} />;
+  }
+  searchSummoner = e => {
+    e.preventDefault();
+    const { summonerName } = this.state;
+    this.props.getSummoner(summonerName);
+  };
+  handleInputChange = e => {
+    this.setState({ summonerName: e.target.value });
+  };
+  render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect {...redirect} />;
 
-        return(
-            <div className={styles.container}>
-          <img
-            src={require("images/Home_img.png")}
-            className={styles.logo}
-             alt='logo'
-          />
+    return (
+      <div className={styles.container}>
+        <img
+          src={require("images/Home_img.png")}
+          className={styles.logo}
+          alt="logo"
+        />
         <form className={styles.summoner_form} onSubmit={this.searchSummoner}>
-            <input className={styles.summoner_search} type='text' value={this.state.summonerName} onChange={this.handleInputChange}/>
-            <button className={styles.summoner_search_btn}>.GG </button>
+          <input
+            className={styles.summoner_search}
+            type="text"
+            value={this.state.summonerName}
+            onChange={this.handleInputChange}
+          />
+          <button className={styles.summoner_search_btn}>.GG </button>
         </form>
-        </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default connect(
-    ({ summoner }) => ({summonerInfo: summoner.search.summonerInfo}),
-    { getSummoner }
-    )(Home)
+  ({ summoner }) => ({ summonerInfo: summoner.search.summonerInfo }),
+  { getSummoner }
+)(Home);
